858861f8398b679da649d126c2ff17847eaf7086
west <413281269@qq.com>
2018-06-11 10:56:05 +0800

    兼容carthage

--- ZSSRichTextEditor/Source/Third Party/HRColorPickerViewController.m
+++ ZSSRichTextEditor/Source/Third Party/HRColorPickerViewController.m
@@ -161,17 +161,4 @@
 #pragma mark - View lifecycle
 
 
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-
 @end
