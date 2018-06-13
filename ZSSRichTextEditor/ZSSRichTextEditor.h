f67a181e677a4e8baa7fbed46f82d80ccb1f69df
west <413281269@qq.com>
2018-06-13 18:50:11 +0800

    适配carthage

---
+++ ZSSRichTextEditor/ZSSRichTextEditor.h
@@ -0,0 +1,25 @@
+//
+//  ZSSRichTextEditor.h
+//  ZSSRichTextEditor
+//
+//  Created by west on 2018/6/11.
+//  Copyright © 2018年 West. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZSSRichTextEditor.h"
+#import "ZSSTextView.h"
+#import "ZSSBarButtonItem.h"
+#import "ZSSFontsViewController.h"
+#im
+
+//! Project version number for ZSSRichTextEditor.
+FOUNDATION_EXPORT double ZSSRichTextEditorVersionNumber;
+
+//! Project version string for ZSSRichTextEditor.
+FOUNDATION_EXPORT const unsigned char ZSSRichTextEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZSSRichTextEditor/PublicHeader.h>
+
+
